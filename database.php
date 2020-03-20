518c57280e86646a87af8908937104ed8cdd1152
ahmadmiftahfajrin <miftah.fr@gmail.com>
2020-03-20 21:39:27 +0700

    Update database.php

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
 $dbhost = getenv("MYSQL_SERVICE_HOST");
 $dbusername = 'root';
 $dbpassword = 'ubaya';
-$dbname = 'sampledb';
+$dbname = 'ubayadb';
 
 $c = new mysqli($dbhost, $dbusername, $dbpassword, $dbname);
 if ($c->connect_error) {
