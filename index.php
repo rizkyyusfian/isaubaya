ee75bc80eb389adb6f7d27330a15786e02a7fef0
rizkyyusfian <yusfian.zoldyck@gmail.com>
2020-03-27 18:04:26 +0700

    Hasil Update commit

--- index.php
+++ index.php
@@ -26,16 +26,17 @@ require_once("database.php");
 ?>
 <body>
 
-  <h2>Driver</h2>
+  <h2>Mahasiswa Ubaya</h2>
 
   <table>
     <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>License</th>
+      <th>Nrp</th>
+      <th>Nama</th>
+      <th>Jurusan</th>
+      <th>Angkatan</th
     </tr>
     <?php
-    $sql = "SELECT * FROM driver ORDER by name ASC";
+    $sql = "SELECT * FROM mahasiswa ORDER by nrp ASC";
     $result = $c->query($sql);
     if ($result->num_rows > 0) {
 
@@ -43,9 +44,10 @@ require_once("database.php");
       $i = 0;
       while ($obj = $result->fetch_assoc()) {
         echo '<tr>';
-        echo '<td>' . $obj['id'] . '</td>';
-        echo '<td>' . $obj['name'] . '</td>';
-        echo '<td>' . $obj['license'] . '</td>';
+        echo '<td>' . $obj['nrp'] . '</td>';
+        echo '<td>' . $obj['nama'] . '</td>';
+        echo '<td>' . $obj['jurusan'] . '</td>';
+        echo '<td>' . $obj['angkatan'] . '</td>';
       }
     } else {
       echo "Empty table.";
